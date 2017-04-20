c23e06a64ba6225bfc3c129bad3bc387f968a125
Michael Bilstein <micha3lbilstein@gmail.com>
2017-04-20 15:36:16 -0400

    giphyyyy

--- giphy.js
+++ giphy.js
@@ -10,5 +10,7 @@ const rp = require('request-promise');
 return rp('http://api.giphy.com/v1/gifs/search?q='+search+'&api_key=dc6zaTOxFJmzC')
 		 .then((res) => {
 			let response = JSON.parse(res);
-			return response.data[0].images.original.url;
+			if (response.data.length > 0)
+				return response.data[0].images.original.url;
+			else return 'Nothing found.'
 		 });

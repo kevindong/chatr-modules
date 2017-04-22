cb1fee4a529be47a68a7a0d1474d8646f46a635a
Michael Bilstein <micha3lbilstein@gmail.com>
2017-04-22 15:57:12 -0400

    fix

--- pig-latin.js
+++ pig-latin.js
@@ -1,1 +1,1 @@
-return message.split(" ").forEach(w => "aeiou".contains(w[0]) ? w.substr(1) + w[0] + "ay" : w + "ay").join(" ");
+return new Promise.resolve(message.split(" ").forEach(w => "aeiou".contains(w[0]) ? w.substr(1) + w[0] + "ay" : w + "ay").join(" "));
